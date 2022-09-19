ce8d8e3b65c9e323e2ec5032b9a1d38da531808f
Rolando Diaz Cruz <rolandoo0@icloud.com>
2022-09-19 14:52:50 -0400

    Update

--- src/SeAPI/se_main.py
+++ src/SeAPI/se_main.py
@@ -1,1 +1,19 @@
-from SeAPI import
+from se_addresses import ShipEngineAddresses
+from se_webhooks import ShipEngineWebhooks
+from se_request import SeRequest
+from se_carriers import ShipEngineCarriers
+from se_labels import ShipEngineLabels
+from se_pickups import ShipEnginePickups
+import json
+
+
+class ShipEngine(
+    ShipEngineAddresses,
+    ShipEngineWebhooks,
+    SeRequest,
+    ShipEngineCarriers,
+    ShipEngineLabels,
+    ShipEnginePickups,
+):
+    def __init__(self, ship_engine_api_key) -> None:
+        self.api_key = ship_engine_api_key
